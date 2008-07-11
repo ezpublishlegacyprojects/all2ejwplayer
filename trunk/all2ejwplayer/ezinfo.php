77c869dbb9e0160318bb6fd4ce53f9ac14e9cd83
nleutner <info@example.com>
2008-07-11 15:18:03 +0000

    git-svn-id: http://svn.projects.ez.no/all2ejwplayer@9 d3144e5c-381b-4772-aff7-bab72db62056

--- trunk/all2ejwplayer/ezinfo.php
+++ trunk/all2ejwplayer/ezinfo.php
@@ -4,14 +4,14 @@ class all2ejwplayerInfo
     /*!
      Constructor
     */
-    function info()
+	function info()
     {
-        return array( 'name' => 'all2e video player',
-                      'version' => '1.x',
-                      'copyright' => 'Copyright © 2008 all2e',
-                      'info_url' => 'http://all2e.com',
-                      'license' => 'Protected' );
+        return array( 'Name' => "JW Player integration",
+                      'Version' => "1.0",
+                      'Copyright' => "Copyright (C) 2008 all2e GmbH",
+                      'License' => "GNU General Public License v2.0"
+                     );
     }
-}
+}    
 
 ?>
