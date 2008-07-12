bc25f57d7ab077a77439508ab0be2c5f570ce598
myxwr <info@example.com>
2008-07-12 06:05:13 +0000

    added link in ezinfo.php to all2e website
    git-svn-id: http://svn.projects.ez.no/all2ejwplayer@10 d3144e5c-381b-4772-aff7-bab72db62056

--- trunk/all2ejwplayer/ezinfo.php
+++ trunk/all2ejwplayer/ezinfo.php
@@ -8,7 +8,7 @@ class all2ejwplayerInfo
     {
         return array( 'Name' => "JW Player integration",
                       'Version' => "1.0",
-                      'Copyright' => "Copyright (C) 2008 all2e GmbH",
+                      'Copyright' => "Copyright (C) 2008 <a href='http://all2e.com'>all2e GmbH</a>",
                       'License' => "GNU General Public License v2.0"
                      );
     }
